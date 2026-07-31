6ac4eadd280a089d1fb9d331002001802c4a54e4
swayamvermaa <swayamverma1999@gmail.com>
2026-08-01 01:18:23 +0530

    updated till 8/1/2026 1:18

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,17 +1,19 @@
-// import admin from "firebase-admin";
-// import serviceAccount from "../serviceAccountKey.json" with { type: "json" };
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
-
-// export default admin;
-
 import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
-import serviceAccount from "../serviceAccountKey.json" with {
-  type: "json",
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url:
+    process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url:
+    process.env.FIREBASE_CLIENT_X509_CERT_URL,
 };
 
 initializeApp({
