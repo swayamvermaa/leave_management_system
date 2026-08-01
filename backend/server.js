5ac956c1e16a9d49f3087b2d3e47e9ab0254ad56
swayamvermaa <swayamverma1999@gmail.com>
2026-08-01 10:25:19 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,41 @@
-import userRoutes from "./routes/userRoutes.js";
+// import userRoutes from "./routes/userRoutes.js";
+// import dotenv from "dotenv";
+// import app from "./app.js";
+// dotenv.config();
+
+// console.log(process.cwd());
+// console.log("BREVO_HOST =", process.env.BREVO_HOST);
+// console.log("BREVO_PORT =", process.env.BREVO_PORT);
+// console.log("BREVO_USER =", process.env.BREVO_USER);
+// import app from "./app.js";
+// import connectDB from "./config/db.js";
+
+// import eventRoutes from "./routes/eventRoutes.js";
+// app.use("/api/events", eventRoutes);
+// app.use("/api/users", userRoutes);
+
+// connectDB();
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
+
 import dotenv from "dotenv";
-import app from "./app.js";
 dotenv.config();
+
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
+import userRoutes from "./routes/userRoutes.js";
+import eventRoutes from "./routes/eventRoutes.js";
 
 console.log(process.cwd());
 console.log("BREVO_HOST =", process.env.BREVO_HOST);
 console.log("BREVO_PORT =", process.env.BREVO_PORT);
 console.log("BREVO_USER =", process.env.BREVO_USER);
-import app from "./app.js";
-import connectDB from "./config/db.js";
 
-import eventRoutes from "./routes/eventRoutes.js";
 app.use("/api/events", eventRoutes);
 app.use("/api/users", userRoutes);
 
@@ -19,5 +44,5 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
